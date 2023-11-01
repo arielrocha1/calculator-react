7ae8a6bef5367e9f22e0e1087f4475081eaf3acb
arielrocha1 <ariel_rocha@live.com>
2023-11-01 12:24:18 -0400

    Feat: Add new calculator 0.1v

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import "./Button.css"
+import React from "react"
+
+export default function Button(props){
+    
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return(
+        <button 
+            onClick={e=> props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
